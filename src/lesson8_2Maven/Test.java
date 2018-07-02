690a9bbbdb658511f489dcb425b88e3195b80be4
Dennis Löhmann <>
2018-07-02 22:08:01 +0200

    Removed advanced modules

--- src/lesson8_2Maven/Test.java
+++
@@ -1,9 +0,0 @@
-package lesson8_2Maven;
-
-/**
- * @Author Dennis Löhmann (Holisticon AG) 19.06.18
- **/
-public class Test {
-
-
-}
--- src/lesson8_3JunitTest/Test.java
+++
@@ -1,9 +0,0 @@
-package lesson8_3JunitTest;
-
-/**
- * @Author Dennis Löhmann (Holisticon AG) 19.06.18
- **/
-public class Test {
-
-
-}
